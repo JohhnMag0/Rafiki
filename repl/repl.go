2409ab487b4d48eb55293f806c5d0912e052f3f6
John Nessuno <johhnnessuno@tutanota.com>
2023-02-24 22:21:36 -0300

    Rafiki Interpreter

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+// Read Eval Print Loop
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"../lexer"
+	"../token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+	}
+
+	line := scanner.Text()
+	lex := lexer.New(line)
+
+	for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+		fmt.Printf("%+v\n", tok)
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,68 @@
+// token/token.go
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers & Literals
+	IDENT = "IDENT" // variables, functions, class ...
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	NOT    = "!"
+	MULT   = "*"
+	DIV    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	LTEQ   = "<="
+	GTEQ   = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	RBRACK = "{"
+	LBRACK = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"funct":  FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
